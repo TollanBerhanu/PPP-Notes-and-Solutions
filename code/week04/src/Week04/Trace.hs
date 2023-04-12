b800f244b360fe987d7708fba4f11aa4024a3384
TollanBerhanu <tollanberhanu@gmail.com>
2023-04-12 12:19:37 +0300

    Added note on week 4 (3rd iteration)

--- code/week04/src/Week04/Trace.hs
+++ code/week04/src/Week04/Trace.hs
@@ -16,12 +16,14 @@ import Week04.Vesting
 -- EmulatorTrace a
 
 test :: IO ()
-test = runEmulatorTraceIO myTrace
+test = runEmulatorTraceIO myTrace -- Runs the emulator trace an displays the result in the console
 
-myTrace :: EmulatorTrace ()
+myTrace :: EmulatorTrace () -- Emulator trace is an instance of monad, so we can use the do notation to bind actions sequentially(>>=)
 myTrace = do
-    h1 <- activateContractWallet (knownWallet 1) endpoints
-    h2 <- activateContractWallet (knownWallet 2) endpoints
+    h1 <- activateContractWallet (knownWallet 1) endpoints -- unlike the plutus playground, we have to manually start the contracts 
+    h2 <- activateContractWallet (knownWallet 2) endpoints -- themselves before just calling the endpoints,... using activateContractWallet
+            -- we give it the wallet (on which to activate this contract) and the contract.. in this case the wallet is a default wallet
+            -- the result we get from this is called a handle
     callEndpoint @"give" h1 $ GiveParams
         { gpBeneficiary = mockWalletPaymentPubKeyHash $ knownWallet 2
         , gpDeadline    = slotToBeginPOSIXTime def 20
