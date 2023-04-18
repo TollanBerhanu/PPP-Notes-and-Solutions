ec736b6134d50707622ab72162aedf3116a95d13
TollanBerhanu <tollanberhanu@gmail.com>
2023-04-18 13:23:25 +0000

    Added comment to Vesting script

--- code/Week03/lecture/Vesting.hs
+++ code/Week03/lecture/Vesting.hs
@@ -60,8 +60,28 @@ saveVal = writeValidatorToFile "./assets/vesting.plutus" validator
 vestingAddressBech32 :: Network -> String
 vestingAddressBech32 network = validatorAddressBech32 network validator
 
+-- Takes the PubKeyHash (beneficiary's pubKeyHash) and the time (deadline) and constructs and displays the datum in a valid format
+
 printVestingDatumJSON :: PubKeyHash -> String -> IO ()
 printVestingDatumJSON pkh time = printDataToJSON $ VestingDatum
     { beneficiary = pkh
     , deadline    = fromJust $ posixTimeFromIso8601 time
     }
+
+-- Prelude Vesting Plutus.V2.Ledger.Api> :set -XOverloadedStrings
+-- Prelude Vesting Plutus.V2.Ledger.Api> pkh2 = PubKeyHash "037fb7436ae85e7354c91c40998667b05083660ebfc675e110c3cf0e"
+-- Prelude Vesting Plutus.V2.Ledger.Api> printVestingDatumJSON pkh2 "2023-04-18T12:00:00.0Z"
+-- {
+--     "constructor": 0,
+--     "fields": [
+--         {
+--             "bytes": "3033376662373433366165383565373335346339316334303939383636376230353038333636306562666336373565313130633363663065"
+--         },
+--         {
+--             "int": 1681819200000
+--         }
+--     ]
+-- }
+
+-- The "bytes" field here shows the Hex version of the actual pubKeyHash, so you should manually replace the "bytes" field
+-- with the pubKeyHash of the beneficiary
