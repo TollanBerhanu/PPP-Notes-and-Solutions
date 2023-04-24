28068e18dc68cb2ff66fa99291857ffda71ee2d6
TollanBerhanu <tollanberhanu@gmail.com>
2023-04-24 09:02:34 +0000

    Added notes on minting policy - Week 5 iteration 4

--- code/Week05/lecture/Free.hs
+++ code/Week05/lecture/Free.hs
@@ -14,21 +14,22 @@ import           Utilities            (currencySymbol, wrapPolicy,
                                        writePolicyToFile)
 
 {-# INLINABLE mkFreePolicy #-}
-mkFreePolicy :: () -> ScriptContext -> Bool
-mkFreePolicy () _ = True
+            -- Redeemer -> Context ... No datum in minting policies, but we can add parameters (Params -> Redeemer -> Context -> Bool)
+mkFreePolicy :: () -> ScriptContext -> Bool     -- Typed version, we don't need a toData instance for the redeemer because it's just ()
+mkFreePolicy () _ = True -- Allow any minting/burning for the CurrencySymbol given by this policy (hash of this minting script)
 
-{-# INLINABLE mkWrappedFreePolicy #-}
-mkWrappedFreePolicy :: BuiltinData -> BuiltinData -> ()
+{-# INLINABLE mkWrappedFreePolicy #-} -- For template haskell, to include all the implementation in the oxford brackets (when we compile it to plutus script)
+mkWrappedFreePolicy :: BuiltinData -> BuiltinData -> ()   -- Helper function to convert the typed policy to Untyped using wrapPolicy
 mkWrappedFreePolicy = wrapPolicy mkFreePolicy
 
 freePolicy :: MintingPolicy
-freePolicy = mkMintingPolicyScript $$(PlutusTx.compile [|| mkWrappedFreePolicy ||])
-
+-- freePolicy = mkMintingPolicyScript $$(PlutusTx.compile [|| wrapPolicy mkFreePolicy ||]) -- without using the above function
+freePolicy = mkMintingPolicyScript $$(PlutusTx.compile [|| mkWrappedFreePolicy ||]) -- Compile it to plutus core (serialized version)
 ---------------------------------------------------------------------------------------------------
 ------------------------------------- HELPER FUNCTIONS --------------------------------------------
 
 saveFreePolicy :: IO ()
-saveFreePolicy = writePolicyToFile "assets/free.plutus" freePolicy
+saveFreePolicy = writePolicyToFile "assets/free.plutus" freePolicy -- write the serialized minting policy to file
 
 freeCurrencySymbol :: CurrencySymbol
-freeCurrencySymbol = currencySymbol freePolicy
+freeCurrencySymbol = currencySymbol freePolicy -- get the CurrencySymbol (hash of the serialized minting policy )
--- code/Week05/lecture/lucid-free.ts
+++ code/Week05/lecture/lucid-free.ts
@@ -11,11 +11,11 @@ import {
 import { blockfrostKey, secretSeed } from "./secret.ts"
 
 function readAmount(): bigint {
-    const input = prompt("amount: ");
-    return input ? BigInt(Number.parseInt(input)) : 1000000n;
+    const input = prompt("amount: "); // asks user to input amount
+    return input ? BigInt(Number.parseInt(input)) : 1000000n; // Return 100000 if the user failed to input amount
 }
 
-const freePolicy: MintingPolicy = {
+const freePolicy: MintingPolicy = { // Our Free minting policy
     type: "PlutusV2",
     script: "5830582e010000323222320053333573466e1cd55ce9baa0024800080148c98c8014cd5ce249035054310000500349848005"
 };
@@ -23,29 +23,39 @@ const freePolicy: MintingPolicy = {
 // set blockfrost endpoint
 const lucid = await Lucid.new(
     new Blockfrost(
-        "https://cardano-preprod.blockfrost.io/api/v0",
+        "https://cardano-preview.blockfrost.io/api/v0",
         blockfrostKey
     ),
-    "Preprod"
+    "Preview"
 );
 
 // load local stored seed as a wallet into lucid
 lucid.selectWalletFromSeed(secretSeed);
 const addr: Address = await lucid.wallet.address();
-console.log("own address: " + addr);
+console.log("own address: " + addr); // log out my public address
 
 const policyId: PolicyId = lucid.utils.mintingPolicyToId(freePolicy);
-console.log("minting policy: " + policyId);
+console.log("minting policy: " + policyId); // log out the PolicyID/hash of serialized minting policy (CurrencySymbol)
 
 const unit: Unit = policyId + fromText("PPP Free");
+    // Unit is the representation of (AssetClass :: (CurrencySymbol, TokenName)) in lucid... it's just the concatenation of the
+        // CurrencySymbol & the Hex representation of the token name (because TokenNames are arbitrary bytestrings (it's easier to 
+        // work with them in Hex form), also cardano-cli expects them in Hex)
 
-const amount: bigint = readAmount();
+const amount: bigint = readAmount(); // read amount to mint from user
 
-const tx = await lucid
+const tx = await lucid // Construct the txn
     .newTx()
-    .mintAssets({[unit]: amount}, Data.void())
-    .attachMintingPolicy(freePolicy)
+    .mintAssets({[unit]: amount}, Data.void()) // mintAssets(assets: Assets, redeemer?: Redeemer): Tx
+        // an object where the keys are the asset classes we want to mint and the value is the amount
+        // all assets must have the same CurrencySymbol / PolicyId
+        // we chain mintAssets functions if we want to mint assets with different policy ids.
+    .attachMintingPolicy(freePolicy) // attachMintingPolicy(mintingPolicy: MintingPolicy): Tx
+        // we attach the actual script / minting policy
+        // we can also use reference scripts like validation scripts but minting policies are normally used only once 
+            // (we normally mint native tokens once)
     .complete();
+    // Lucid automatically includes UTxO from our wallet to pay for txn fees
 
 const signedTx = await tx.sign().complete();
 const txHash = await signedTx.submit();
