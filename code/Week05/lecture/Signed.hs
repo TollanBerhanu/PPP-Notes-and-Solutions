4083508e12284765937c3335090ad1143e80c623
TollanBerhanu <tollanberhanu@gmail.com>
2023-04-25 07:09:42 +0000

    Added notes on week 5 - Signed minting policy

--- code/Week05/lecture/Signed.hs
+++ code/Week05/lecture/Signed.hs
@@ -23,20 +23,23 @@ mkSignedPolicy pkh () ctx = traceIfFalse "missing signature" $ txSignedBy (scrip
 
 {-# INLINABLE mkWrappedSignedPolicy #-}
 mkWrappedSignedPolicy :: BuiltinData -> BuiltinData -> BuiltinData -> () -- we used BuiltinData as the first param instead of PubKeyHash
+    -- because we can only use parameters of type BuiltinData in lucid
 mkWrappedSignedPolicy pkh = wrapPolicy (mkSignedPolicy $ PlutusTx.unsafeFromBuiltinData pkh)
-                                                        -- we used unsafeFromBuiltinData to convert from BuiltinData to PubKeyHash
+                                    -- we used unsafeFromBuiltinData to convert from BuiltinData to PubKeyHash to feed it to mkSignedPolicy
 
--- Here we did the compilation in two steps: compilation and application of the parameters
+-- Here we did the compilation in two steps: compilation and application of the parameters to get two versions of the serialized script
 signedCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> ())
-signedCode = $$(PlutusTx.compile [|| mkWrappedSignedPolicy ||])
+signedCode = $$(PlutusTx.compile [|| mkWrappedSignedPolicy ||]) -- mkWrappedSignedPolicy expects an additional parameter, we compile this
+    -- function without the parameter so that we can apply the param on the off-chain side (using lucid)
 
-signedPolicy :: PubKeyHash -> MintingPolicy -- add a parameter (pubKeyHash) to the MintingPolicy
+signedPolicy :: PubKeyHash -> MintingPolicy -- add a parameter (pubKeyHash) to the MintingPolicy when running this function on-chain
 signedPolicy pkh = mkMintingPolicyScript $ signedCode `PlutusTx.applyCode` PlutusTx.liftCode (PlutusTx.toBuiltinData pkh)
-                                                            -- we convert PubKeyHash to BuiltinData when we use it as a parameter
+                                            -- we convert PubKeyHash to BuiltinData when we use it as a parameter on the on-chain code
 ---------------------------------------------------------------------------------------------------
 ------------------------------------- HELPER FUNCTIONS --------------------------------------------
 
--- This writes the serialized script where the parameter hasn't been applied yet, so we can use lucid (off-chain code) to apply the parameter
+-- This writes the serialized script where the parameter hasn't been applied yet, so we can use lucid (off-chain code) to apply the 
+    -- parameter. Keep in mind that it's not a complete minting policy until we apply the parameter
 saveSignedCode :: IO ()
 saveSignedCode = writeCodeToFile "assets/signed.plutus" signedCode
 
