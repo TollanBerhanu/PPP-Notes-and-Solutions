027bfb4ae4e583c4e7042e28cf9b4415a87d626f
TollanBerhanu <tollanberhanu@gmail.com>
2023-04-25 12:33:19 +0000

    Completed week 5 homework 1

--- code/Week05/homework/Homework1.hs
+++ code/Week05/homework/Homework1.hs
@@ -1,28 +1,62 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Homework1 where
 
-import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy, POSIXTime,
-                                       PubKeyHash, ScriptContext,
+import           Plutus.V1.Ledger.Interval (after)
+import           Plutus.V2.Ledger.Api (BuiltinData, MintingPolicy, POSIXTime, TxInfo (txInfoValidRange),
+                                       PubKeyHash, ScriptContext(scriptContextTxInfo),
                                        mkMintingPolicyScript)
+import           Plutus.V2.Ledger.Contexts (txSignedBy)                                       
 import qualified PlutusTx
-import           PlutusTx.Prelude     (Bool (False), ($))
-import           Utilities            (wrapPolicy)
+import           PlutusTx.Prelude     (Bool (False), ($), (&&), traceIfFalse)
+import           Prelude              (IO, Show (show))
+import           Text.Printf          (printf)
+import           Utilities            (wrapPolicy, writeCodeToFile)
 
 {-# INLINABLE mkDeadlinePolicy #-}
 -- This policy should only allow minting (or burning) of tokens if the owner of the specified PubKeyHash
 -- has signed the transaction and if the specified deadline has not passed.
 mkDeadlinePolicy :: PubKeyHash -> POSIXTime -> () -> ScriptContext -> Bool
-mkDeadlinePolicy _pkh _deadline () _ctx = False -- FIX ME!
+mkDeadlinePolicy _pkh _deadline () _ctx = traceIfFalse "Owner hasn't signed" ownerSigned && -- FIXED!
+                                          traceIfFalse "Deadline has passed" deadlineNotReached
+    where
+        info :: TxInfo
+        info = scriptContextTxInfo _ctx
+        
+        ownerSigned :: Bool
+        ownerSigned = txSignedBy info _pkh
+
+        deadlineNotReached :: Bool
+        deadlineNotReached = _deadline `after` (txInfoValidRange info) 
+
+-- ====== This is the way to apply the arguments off-chain
 
 {-# INLINABLE mkWrappedDeadlinePolicy #-}
-mkWrappedDeadlinePolicy :: PubKeyHash -> POSIXTime -> BuiltinData -> BuiltinData -> ()
-mkWrappedDeadlinePolicy pkh deadline = wrapPolicy $ mkDeadlinePolicy pkh deadline
+mkWrappedDeadlinePolicy :: BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ()
+mkWrappedDeadlinePolicy pkh deadline = wrapPolicy $ mkDeadlinePolicy (PlutusTx.unsafeFromBuiltinData pkh) (PlutusTx.unsafeFromBuiltinData deadline)
 
-deadlinePolicy :: PubKeyHash -> POSIXTime -> MintingPolicy
-deadlinePolicy pkh deadline = mkMintingPolicyScript $
-    $$(PlutusTx.compile [|| mkWrappedDeadlinePolicy ||])
-        `PlutusTx.applyCode` PlutusTx.liftCode pkh
-        `PlutusTx.applyCode` PlutusTx.liftCode deadline
+deadlineCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ())
+deadlineCode = $$(PlutusTx.compile [|| mkWrappedDeadlinePolicy ||])
+
+saveDeadlineCode :: IO ()
+saveDeadlineCode = writeCodeToFile "assets/deadline.plutus" deadlineCode
+
+{- 
+-- ====== This is the way to apply the arguments on-chain
+
+    {-# INLINABLE mkWrappedDeadlinePolicy #-}
+    mkWrappedDeadlinePolicy :: PubKeyHash -> POSIXTime -> BuiltinData -> BuiltinData -> ()
+    mkWrappedDeadlinePolicy pkh deadline = wrapPolicy $ mkDeadlinePolicy pkh deadline
+
+    deadlinePolicy :: PubKeyHash -> POSIXTime -> MintingPolicy
+    deadlinePolicy pkh deadline = mkMintingPolicyScript $
+        $$(PlutusTx.compile [|| mkWrappedDeadlinePolicy ||])
+            `PlutusTx.applyCode` PlutusTx.liftCode pkh
+            `PlutusTx.applyCode` PlutusTx.liftCode deadline
+
+    saveDeadlinePolicy :: PubKeyHash -> POSIXTime -> IO ()
+    saveDeadlinePolicy pkh deadline = writePolicyToFile (printf "assets/deadline-%s.plutus" $ show pkh) $ deadlinePolicy pkh deadline
+-}
--- code/Week05/lecture/NFT.hs
+++ code/Week05/lecture/NFT.hs
@@ -27,36 +27,46 @@ import           Utilities                  (bytesToHex, currencySymbol,
                                              writePolicyToFile)
 
 {-# INLINABLE mkNFTPolicy #-}
-mkNFTPolicy :: TxOutRef -> TokenName -> () -> ScriptContext -> Bool
+                -- Param1 -> Param2 -> Redeemer -> Context
+mkNFTPolicy :: TxOutRef -> TokenName -> () -> ScriptContext -> Bool  -- our minting policy takes 2 params: UTxO and TokenName
 mkNFTPolicy oref tn () ctx = traceIfFalse "UTxO not consumed"   hasUTxO           &&
                              traceIfFalse "wrong amount minted" checkMintedAmount
   where
     info :: TxInfo
     info = scriptContextTxInfo ctx
 
-    hasUTxO :: Bool
-    hasUTxO = any (\i -> txInInfoOutRef i == oref) $ txInfoInputs info
+    hasUTxO :: Bool -- check if the specified UTxO is consumed by the txn 
+    hasUTxO = any (\i -> txInInfoOutRef i == oref) $ txInfoInputs info -- get all input UTxOs and check whether any of them is our UTxO
+                                                    -- txInfoInputs :: TxInfo -> [TxInInfo]
+                                                    -- txInInfoOutRef :: TxInInfo -> TxOutRef
 
-    checkMintedAmount :: Bool
-    checkMintedAmount = case flattenValue (txInfoMint info) of
-        [(_, tn'', amt)] -> tn'' == tn && amt == 1
-        _                -> False
+    checkMintedAmount :: Bool -- check if the amount of tokens (NFTs) minted is only ONE.
+    checkMintedAmount = case flattenValue (txInfoMint info) of   -- flattenValue :: Value -> [(CurrencySymbol, TokenName, Integer)]
+        [(_, tn'', amt)] -> tn'' == tn && amt == 1  -- check if the TokenName is the same and if the amount is 1
+        _                -> False 
 
 {-# INLINABLE mkWrappedNFTPolicy #-}
+-- We make the params' type BuiltinData because thats what is required in lucid. We also used two params instead of TxOutRef so that 
+-- we can pass the TxHash and the Index as different parameters (also required by lucid because we can't directly convert Builtindata to
+-- TxOutRef)
+                         -- TxId -> Idx -> TokenName -> BuiltinData -> BuiltinData -> ()
 mkWrappedNFTPolicy :: BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkWrappedNFTPolicy tid ix tn' = wrapPolicy $ mkNFTPolicy oref tn
   where
     oref :: TxOutRef
-    oref = TxOutRef
-        (TxId $ PlutusTx.unsafeFromBuiltinData tid)
-        (PlutusTx.unsafeFromBuiltinData ix)
+    oref = TxOutRef -- compute the TxOutRef from TxId and Idx ... TxOutRef :: TxId -> Integer -> TxOutRef
+        (TxId $ PlutusTx.unsafeFromBuiltinData tid) -- convert the Builtindata back to BuiltinByteString ... TxId :: BuiltinByteString -> TxId
+        (PlutusTx.unsafeFromBuiltinData ix)         -- convert the Builtindata back to Integer
 
     tn :: TokenName
-    tn = PlutusTx.unsafeFromBuiltinData tn'
+    tn = PlutusTx.unsafeFromBuiltinData tn'   -- convert the Builtindata back to TokenName ... TokenName :: BuiltinByteString -> TokenName
+-- Both TxId and TokenName are newType wrappers around BuiltinBytestring but they are deserialized differently, for some reason.
 
+-- This yields the parameterized script (script that expects parameters) ... still not a complete minting policy
 nftCode :: PlutusTx.CompiledCode (BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> BuiltinData -> ())
 nftCode = $$(PlutusTx.compile [|| mkWrappedNFTPolicy ||])
 
+-- This yields the actual minting policy by applying the parameters
 nftPolicy :: TxOutRef -> TokenName -> MintingPolicy
 nftPolicy oref tn = mkMintingPolicyScript $
     nftCode
@@ -66,10 +76,11 @@ nftPolicy oref tn = mkMintingPolicyScript $
 
 ---------------------------------------------------------------------------------------------------
 ------------------------------------- HELPER FUNCTIONS --------------------------------------------
-
+-- Save the parameterized script to file
 saveNFTCode :: IO ()
 saveNFTCode = writeCodeToFile "assets/nft.plutus" nftCode
 
+-- save the minting policy to file after accepting the TxOutRef and TokenName on the on-chain side
 saveNFTPolicy :: TxOutRef -> TokenName -> IO ()
 saveNFTPolicy oref tn = writePolicyToFile
     (printf "assets/nft-%s#%d-%s.plutus"
@@ -82,5 +93,6 @@ saveNFTPolicy oref tn = writePolicyToFile
     tn' = case unTokenName tn of
         (BuiltinByteString bs) -> BS8.unpack $ bytesToHex bs
 
+-- Show the CurrencySymbol after accepting parameters
 nftCurrencySymbol :: TxOutRef -> TokenName -> CurrencySymbol
 nftCurrencySymbol oref tn = currencySymbol $ nftPolicy oref tn
